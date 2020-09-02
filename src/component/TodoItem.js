e86487d2765dcabb3eb6559c7287318ee7c4554f
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-09-02 18:05:12 +0530

    Added event listener instead of css on hover properties

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -7,6 +7,8 @@ class TodoItem extends React.Component {
     this.state = { isDeleteIconVisible: false };
     this.handleClick = this.handleClick.bind(this);
     this.delete = this.delete.bind(this);
+    this.showDeleteIcon = this.showDeleteIcon.bind(this);
+    this.hideDeleteIcon = this.hideDeleteIcon.bind(this);
   }
 
   handleClick(event) {
@@ -17,20 +19,32 @@ class TodoItem extends React.Component {
     this.props.delete(id);
   }
 
+  showDeleteIcon() {
+    this.setState({ isDeleteIconVisible: true });
+  }
+
+  hideDeleteIcon() {
+    this.setState({ isDeleteIconVisible: false });
+  }
+
   render() {
     const { id, text, status } = this.props.todo;
     const classes = `task ${status}`;
+    let deleteElement;
+    if (this.state.isDeleteIconVisible) {
+      deleteElement = <Delete delete={() => this.delete(id)} />;
+    }
     return (
-      <div className={classes}>
+      <div
+        className={classes}
+        onMouseOver={this.showDeleteIcon}
+        onMouseLeave={this.hideDeleteIcon}
+      >
         <div className="indicator"></div>
         <span className="todoText" id={id} onClick={this.handleClick}>
           {text}
         </span>
-        <Delete
-          delete={() => {
-            this.delete(id);
-          }}
-        />
+        {deleteElement}
       </div>
     );
   }
